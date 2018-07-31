aa319354b3f4c9718350a2a63dcfb0f930f70125
Johann Brehmer <mail@johannbrehmer.de>
2018-07-31 10:47:49 -0400

    Added DelphesMiner package content to MadMiner package

---
+++ delphesprocessor/delphesprocessor.py
@@ -0,0 +1,129 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from collections import OrderedDict
+import numpy as np
+import logging
+
+from delphesprocessor.tools.h5_interface import save_madminer_file
+from delphesprocessor.tools.delphes_interface import run_delphes
+from delphesprocessor.tools.root_interface import extract_observables_from_delphes_file
+from delphesprocessor.tools.utils import general_init
+
+
+class DelphesProcessor:
+    """ """
+
+    def __init__(self, debug=False):
+        """ Constructor """
+
+        general_init(debug=debug)
+
+        # Initialize samples
+        self.hepmc_sample_filenames = []
+        self.delphes_sample_filenames = []
+
+        # Initialize observables
+        self.observables = OrderedDict()
+        self.observables_required = OrderedDict()
+
+        # Initialize samples
+        self.observations = None
+        self.weights = None
+
+    def add_hepmc_sample(self, filename):
+
+        logging.info('Adding HepMC sample at %s', filename)
+
+        self.hepmc_sample_filenames.append(filename)
+
+    def run_delphes(self, delphes_directory, delphes_card, initial_command=None, log_directory=None):
+
+        logging.info('Running Delphes at %s', delphes_directory)
+
+        if log_directory is None:
+            log_directory = './logs'
+        log_file = log_directory + '/delphes.log'
+
+        for hepmc_sample_filename in self.hepmc_sample_filenames:
+            delphes_sample_filename = run_delphes(
+                delphes_directory,
+                delphes_card,
+                hepmc_sample_filename,
+                initial_command=initial_command,
+                log_file=log_file
+            )
+            self.delphes_sample_filenames.append(delphes_sample_filename)
+
+    def add_delphes_sample(self, filename):
+
+        logging.info('Adding Delphes sample at %s', filename)
+
+        self.delphes_sample_filenames.append(filename)
+
+    def add_observable(self, name, definition, required=False):
+
+        if required:
+            logging.info('Adding required observable %s = %s', name, definition)
+        else:
+            logging.info('Adding (not required) observable %s = %s', name, definition)
+
+        self.observables[name] = definition
+        self.observables_required[name] = required
+
+    def read_observables_from_file(self, filename):
+        raise NotImplementedError
+
+    def set_default_observables(self):
+        raise NotImplementedError
+
+    def analyse_delphes_samples(self):
+
+        for delphes_file in self.delphes_sample_filenames:
+
+            logging.info('Analysing Delphes sample %s', delphes_file)
+
+            # Calculate observables and weights
+            this_observations, this_weights = extract_observables_from_delphes_file(
+                delphes_file,
+                self.observables,
+                self.observables_required
+            )
+
+            # Merge
+            if self.observations is None and self.weights is None:
+                self.observations = this_observations
+                self.weights = this_weights
+                continue
+
+            if self.weights.shape[0] != this_weights.shape[0]:
+                raise ValueError("Number of weights in different Delphes files incompatible: {} vs {}".format(
+                    self.weights.shape[0], this_weights.shape[0]
+                ))
+            if self.observations.shape[0] != this_observations.shape[0]:
+                raise ValueError("Number of observations in different Delphes files incompatible: {} vs {}".format(
+                    self.observations.shape[0], this_observations.shape[0]
+                ))
+
+            self.weights = np.hstack([self.weights, this_weights])
+
+            for key in self.observations:
+                assert key in this_observations, "Observable {} not found in Delphes sample!".format(
+                    key
+                )
+                self.observations[key] = np.hstack([self.observations[key], this_observations[key]])
+
+    def save(self, filename_out, filename_in=None):
+
+        assert (self.observables is not None and self.observations is not None
+                and self.weights is not None), 'Nothing to save!'
+
+        if filename_in is None:
+            logging.info('Saving HDF5 file to %s', filename_out)
+        else:
+            logging.info('Loading HDF5 data from %s and saving file to %s', filename_in, filename_out)
+
+        save_madminer_file(filename_out,
+                           self.observables,
+                           self.observations,
+                           self.weights,
+                           copy_from=filename_in)
---
+++ delphesprocessor/tools/delphes_interface.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+
+from delphesprocessor.tools.utils import call_command
+
+
+def run_delphes(delphes_directory,
+                delphes_card_filename,
+                hepmc_sample_filename,
+                delphes_sample_filename=None,
+                initial_command=None,
+                log_file=None):
+
+    # Untar event file
+    filename, extension = os.path.splitext(hepmc_sample_filename)
+    if extension == '.gz':
+        if not os.path.exists(filename):
+            call_command('gunzip -k {}'.format(hepmc_sample_filename))
+        hepmc_sample_filename = filename
+
+    # Where to put Delphes sample
+    if delphes_sample_filename is None:
+        delphes_sample_filename = None
+
+        filename_prefix = hepmc_sample_filename.replace('.hepmc.gz', '')
+        filename_prefix = filename_prefix.replace('.hepmc', '')
+
+        for i in range(1, 1000):
+            if i == 1:
+                filename_candidate = filename_prefix + '_delphes.root'
+            else:
+                filename_candidate = filename_prefix + '_delphes_' + str(i) + '.root'
+
+            if not os.path.exists(filename_candidate):
+                delphes_sample_filename = filename_candidate
+                break
+
+        assert delphes_sample_filename is not None, "Could not find filename for Delphes sample"
+        assert not os.path.exists(delphes_sample_filename), "Could not find filename for Delphes sample"
+
+    # Initial commands
+    if initial_command is None:
+        initial_command = ''
+    else:
+        initial_command = initial_command + '; '
+
+    # Call Delphes
+    _ = call_command(
+        '{}{}/DelphesHepMC {} {} {}'.format(
+            initial_command, delphes_directory,
+            delphes_card_filename, delphes_sample_filename, hepmc_sample_filename
+        ),
+        log_file=log_file
+    )
+
+    return delphes_sample_filename
