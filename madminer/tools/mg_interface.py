c62c4e564a3cb3c70db51c664255412b13a1ed5c
FelixKling <fkling@uci.edu>
2018-08-16 12:18:42 -0700

    Option to not run Pythia
    I added an Option to not run pythia, if the pythia8card is not specified. That's intended for the parton-level only mode.

--- madminer/tools/mg_interface.py
+++ madminer/tools/mg_interface.py
@@ -138,21 +138,13 @@ def run_mg_pythia(mg_directory,
                   initial_command=None,
                   log_file=None):
 
-    # Copy cards
-    if run_card_file is not None:
-        shutil.copyfile(run_card_file, mg_process_directory + '/Cards/run_card.dat')
-    if param_card_file is not None:
-        shutil.copyfile(param_card_file, mg_process_directory + '/Cards/param_card.dat')
-    if reweight_card_file is not None:
-        shutil.copyfile(reweight_card_file, mg_process_directory + '/Cards/reweight_card.dat')
-    if pythia8_card_file is not None:
-        shutil.copyfile(pythia8_card_file, mg_process_directory + '/Cards/pythia8_card.dat')
-
     # Remove unneeded cards
     if os.path.isfile(mg_process_directory + '/Cards/delphes_card.dat'):
         os.remove(mg_process_directory + '/Cards/delphes_card.dat')
     if os.path.isfile(mg_process_directory + '/Cards/pythia_card.dat'):
         os.remove(mg_process_directory + '/Cards/pythia_card.dat')
+    if os.path.isfile(mg_process_directory + '/Cards/pythia_card.dat'):
+        os.remove(mg_process_directory + '/Cards/pythia8_card.dat')
     if os.path.isfile(mg_process_directory + '/Cards/madanalysis5_hadron_card.dat'):
         os.remove(mg_process_directory + '/Cards/madanalysis5_hadron_card.dat')
     if os.path.isfile(mg_process_directory + '/Cards/madanalysis5_parton_card.dat'):
@@ -164,7 +156,7 @@ def run_mg_pythia(mg_directory,
         os.remove(mg_process_directory + '/index.html')
     if os.path.isfile(mg_process_directory + '/crossx.html'):
         os.remove(mg_process_directory + '/crossx.html')
-
+    
     if os.path.isdir(mg_process_directory + '/HTML'):
         shutil.rmtree(mg_process_directory + '/HTML')
     if os.path.isdir(mg_process_directory + '/Events'):
@@ -176,19 +168,40 @@ def run_mg_pythia(mg_directory,
 
     os.mkdir(mg_process_directory + '/HTML')
     os.mkdir(mg_process_directory + '/Events')
+    
+    # Copy cards
+    if run_card_file is not None:
+        shutil.copyfile(run_card_file, mg_process_directory + '/Cards/run_card.dat')
+    if param_card_file is not None:
+        shutil.copyfile(param_card_file, mg_process_directory + '/Cards/param_card.dat')
+    if reweight_card_file is not None:
+        shutil.copyfile(reweight_card_file, mg_process_directory + '/Cards/reweight_card.dat')
+    if pythia8_card_file is not None:
+        shutil.copyfile(pythia8_card_file, mg_process_directory + '/Cards/pythia8_card.dat')
 
     # MG commands
     temp_proc_card_file = temp_directory + '/run.mg5'
 
-    mg_commands = '''
-    launch {}
-    shower=Pythia8
-    detector=OFF
-    analysis=OFF
-    madspin=OFF
-    reweight=ON
-    done
-    '''.format(mg_process_directory)
+    if pythia8_card_file != None:
+        mg_commands = '''
+            launch {}
+            shower=Pythia8
+            detector=OFF
+            analysis=OFF
+            madspin=OFF
+            reweight=ON
+            done
+            '''.format(mg_process_directory)
+    else:
+        mg_commands = '''
+            launch {}
+            shower=OFF
+            detector=OFF
+            analysis=OFF
+            madspin=OFF
+            reweight=ON
+            done
+            '''.format(mg_process_directory)
 
     with open(temp_proc_card_file, 'w') as file:
         file.write(mg_commands)
