a33ef736326dd085eda3a7910616d1267165e3cc
FelixKling <fkling@uci.edu>
2018-08-21 21:50:05 -0700

    Update MadFisher
    Updated MadFisher: It can now calculate the full Fisher info after cuts. New functions are
    - calculate_truth_fisher_information_full
    - passed_cuts
    - calculate_fisher_information
    - extract_observables_and_weights
    - extract_raw_data

--- madminer/madfisher.py
+++ madminer/madfisher.py
@@ -4,6 +4,7 @@ import logging
 import numpy as np
 import collections
 import six
+from collections import OrderedDict
 
 from madminer.tools.h5_interface import load_madminer_settings, madminer_event_loader, save_events_to_madminer_file
 from madminer.tools.analysis import get_theta_value, get_theta_benchmark_matrix, get_dtheta_benchmark_matrix
@@ -51,8 +52,112 @@ class MadFisher:
         else:
             raise ValueError('Did not find morphing setup.')
 
-    def calculate_truth_fisher_information(self):
-        raise NotImplementedError
+
+    def extract_raw_data(self):
+        
+        # This Functio cuts returns the raw data: a list of observables x and weights for the morphing benchmarks for each event
+        x, weights_benchmarks = next(madminer_event_loader(self.madminer_filename, batch_size=None))
+        return x, weights_benchmarks
+    
+    def extract_observables_and_weights(self, thetas=None):
+        
+        # This function returns a list of observables and weights for the benchmark 'theta'
+        """
+        :param theta: list (theta) of list (components of theta) of float
+        :return: list (events) of list (observables) of float, list (event) of list (theta) of float
+        """
+        
+        x, weights_benchmarks = next(madminer_event_loader(self.madminer_filename, batch_size=None))
+
+        weights_thetas = []
+        for theta in thetas:
+            theta_matrix = get_theta_benchmark_matrix('morphing',
+                                                      theta,
+                                                      self.benchmarks,
+                                                      self.morpher
+                                                      )
+            weights_thetas.append( theta_matrix.dot(weights_benchmarks.T) )
+                    
+        return x, weights_thetas
+            
+    def calculate_fisher_information(self,theta,weights_benchmarks,luminosity):
+
+        # This Function calculated a list of Fisher Info Tensors for a given benchmark 'theta' and luminosity
+        """
+        :param theta: list (components of theta) of float
+        :param weights_benchmarks: list (events) of lists (morphing benchmarks) of floats
+        :param luminosity: luminosity in fb^-1, float
+        :return: list (events) of fisher_info (nxn tensor)
+        """
+        
+        #get morphing matrices
+        theta_matrix = get_theta_benchmark_matrix('morphing',
+                                                  theta,
+                                                  self.benchmarks,
+                                                  self.morpher
+                                                  )
+        dtheta_matrix = get_dtheta_benchmark_matrix('morphing',
+                                                    theta,
+                                                    self.benchmarks,
+                                                    self.morpher
+                                                    )
+                                                    
+        #get theta, dtheta for this events
+        sigma = theta_matrix.dot(weights_benchmarks.T)
+        dsigma = dtheta_matrix.dot(weights_benchmarks.T)
+    
+        #calculate fisher info for this event
+        fisher_info = []
+        for i in range(len(sigma)):
+            fisher_info.append(luminosity/sigma[i]*np.tensordot(dsigma.T[i],dsigma.T[i],axes=0))
+        return fisher_info
+    
+    def passed_cuts(self,observables,cuts):
+        
+        # This function checks if an events, specified by a list of observables, passes a set of cuts.
+        """
+        :param observables: list (observables) of float
+        :param cuts: list (cuts) of definition of cuts (string)
+        :return: True if the event passes all cuts, False otherwise
+        """
+        
+        event_observables = OrderedDict()
+        i=0
+        for key, _ in self.observables.items():
+            event_observables[key]=observables[i]
+            i+=1
+
+        for cut in cuts:
+            if eval(cut, event_observables) == False:
+                return False
+        return True
+    
+    
+    def calculate_truth_fisher_information_full(self, theta, luminosity, cuts):
+        
+        # This Function returns a list of observables and Fisher Info Tensors for a given benchmark 'theta' and luminosity, requiring that the events pass a set of cuts
+        """
+        :param theta: list (components of theta) of float
+        :param luminosity: luminosity in fb^-1, float
+        :param cuts: list (cuts) of definition of cuts (string)
+        :return: list (events) of list(observables), fisher_info (nxn tensor)
+        """
+        
+        # Get raw data
+        x_raw, weights_benchmarks = next(madminer_event_loader(self.madminer_filename, batch_size=None))
+        
+        # Get Fisher Info
+        fisher_info_raw = self.calculate_fisher_information(theta,weights_benchmarks,luminosity)
+        
+        # cuts
+        x=[]
+        fisher_info=[]
+        for i in range(len(x_raw)):
+            if self.passed_cuts(x_raw[i],cuts):
+                x.append( x_raw[i] )
+                fisher_info.append(fisher_info_raw[i])
+    
+        return  x, fisher_info
 
     def calculate_histogram_fisher_information(self):
         raise NotImplementedError
