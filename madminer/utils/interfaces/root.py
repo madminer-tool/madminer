bc64cc78bc624463837706cb09d46dec3609807e
Johann Brehmer <mail@johannbrehmer.de>
2018-10-12 11:15:03 -0600

    Fixed issues with Jets in DelphesProcessor

--- madminer/utils/interfaces/root.py
+++ madminer/utils/interfaces/root.py
@@ -290,14 +290,14 @@ def _get_particles_jets(tree, pt_min, eta_max):
     pts = tree.array('Jet.PT')
     etas = tree.array('Jet.Eta')
     phis = tree.array('Jet.Phi')
-    es = tree.array('Jet.E')
+    masses = tree.array('Jet.Mass')
 
     all_particles = []
 
     for ievent in range(len(pts)):
         event_particles = []
 
-        for pt, eta, phi, e in zip(pts[ievent], etas[ievent], phis[ievent], es[ievent]):
+        for pt, eta, phi, mass in zip(pts[ievent], etas[ievent], phis[ievent], masses[ievent]):
 
             if pt_min is not None and pt < pt_min:
                 continue
@@ -305,7 +305,7 @@ def _get_particles_jets(tree, pt_min, eta_max):
                 continue
 
             particle = MadMinerParticle()
-            particle.setptetaphie(pt, eta, phi, e)
+            particle.setptetaphim(pt, eta, phi, mass)
             particle.set_pdgid(9)
             event_particles.append(particle)
 
