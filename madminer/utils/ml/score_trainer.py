Fixed bug in gradient, better logging for score trainer

madminer/utils/ml/score_trainer.py
@@ -117,8 +117,9 @@ def train_local_score_model(
 
     # Regularization
     if grad_x_regularization is not None:
+        n_losses += 1
         loss_weights.append(grad_x_regularization)
-        loss_labels.append('l2_grad_x')
+        loss_labels.append("l2_grad_x")
 
     # Losses over training
     individual_losses_train = []
@@ -164,7 +165,7 @@ def train_local_score_model(
 
             losses = [loss_function(t_hat, t_xz) for loss_function in loss_functions]
             if grad_x_regularization:
-                losses.append(torch.mean(torch.sum(x_gradient**2, dim=1)))
+                losses.append(torch.mean(torch.sum(x_gradient ** 2, dim=1)))
 
             loss = loss_weights[0] * losses[0]
             for _w, _l in zip(loss_weights[1:], losses[1:]):
@@ -191,9 +192,14 @@ def train_local_score_model(
         # Validation
         if validation_split is None:
             if n_epochs_verbose is not None and n_epochs_verbose > 0 and (epoch + 1) % n_epochs_verbose == 0:
+                individual_loss_string = ""
+                for i, (label, value) in enumerate(zip(loss_labels, individual_losses_train[-1])):
+                    if i > 0:
+                        individual_loss_string += ", "
+                    individual_loss_string += "{}: {:4f}".format(label, value)
+
                 logging.info(
-                    "  Epoch %d: train loss %.2f (%s)"
-                    % (epoch + 1, total_losses_train[-1], individual_losses_train[-1])
+                    "  Epoch %d: train loss %.4f (%s)" % (epoch + 1, total_losses_train[-1], individual_loss_string)
                 )
             continue
 
@@ -234,14 +240,25 @@ def train_local_score_model(
         # Print out information
         if n_epochs_verbose is not None and n_epochs_verbose > 0 and (epoch + 1) % n_epochs_verbose == 0:
             if early_stopping and epoch == early_stopping_epoch:
+                individual_loss_string_train = ""
+                individual_loss_string_val = ""
+                for i, (label, value_train, value_val) in enumerate(
+                    zip(loss_labels, individual_losses_train[-1], individual_losses_val[-1])
+                ):
+                    if i > 0:
+                        individual_loss_string_train += ", "
+                        individual_loss_string_val += ", "
+                    individual_loss_string_train += "{}: {:4f}".format(label, value_train)
+                    individual_loss_string_val += "{}: {:4f}".format(label, value_val)
+
                 logging.info(
-                    "  Epoch %d: train loss %.2f (%s), validation loss %.2f (%s) (*)"
+                    "  Epoch %d: train loss %.4f (%s), validation loss %.4f (%s) (*)"
                     % (
                         epoch + 1,
                         total_losses_train[-1],
-                        individual_losses_train[-1],
+                        individual_loss_string_train,
                         total_losses_val[-1],
-                        individual_losses_val[-1],
+                        individual_loss_string_val,
                     )
                 )
             else: