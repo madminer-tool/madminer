9c2aae713e0f418a799fa7ff7d0dd253fc2a2305
Johann Brehmer <mail@johannbrehmer.de>
2018-11-06 09:19:08 -0500

    Set default early_stopping_patience to None

--- madminer/utils/ml/score_trainer.py
+++ madminer/utils/ml/score_trainer.py
@@ -47,7 +47,7 @@ def train_local_score_model(
     double_precision=False,
     validation_split=0.2,
     early_stopping=True,
-    early_stopping_patience=20,
+    early_stopping_patience=None,
     grad_x_regularization=None,
     learning_curve_folder=None,
     learning_curve_filename=None,
