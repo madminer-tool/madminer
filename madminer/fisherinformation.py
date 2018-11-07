Removed next-to-overflow bins for automatic binning, fixed issues in plot_fisher_information_contours_2d(), new option to not calculate covariance for EnsembleForge.evaluate()

madminer/fisherinformation.py
@@ -538,12 +538,11 @@ class FisherInformation:
         if efficiency_functions is None:
             efficiency_functions = []
 
-        # Number of bins
-        n_bins_total = nbins + 2
-
         # Automatic dynamic binning
         dynamic_binning = histrange is None
         if dynamic_binning:
+            n_bins_total = nbins
+
             # Quantile values
             quantile_values = np.linspace(0.0, 1.0, nbins + 1)
 
@@ -570,13 +569,13 @@ class FisherInformation:
 
             # Bin boundaries
             bin_boundaries = weighted_quantile(histo_observables_pilot, quantile_values, weight_theta_pilot)
-            bin_boundaries[0] -= 10.0
-            bin_boundaries[-1] += 10.0
+            bin_boundaries = bin_boundaries[1:-1]
 
             logging.debug("Automatic dynamic binning: bin boundaries %s", bin_boundaries)
 
         # Manual binning
         else:
+            n_bins_total = nbins + 2
             bin_boundaries = np.linspace(histrange[0], histrange[1], num=nbins + 1)
 
         # Loop over batches

madminer/ml.py
@@ -1151,6 +1151,7 @@ class EnsembleForge:
         theta1_filename=None,
         test_all_combinations=True,
         vote_expectation_weight=None,
+        calculate_covariance=True,
         return_individual_predictions=False,
     ):
 
@@ -1191,6 +1192,9 @@ class EnsembleForge:
             list. If None, or if `calculate_expectation()` has not been called, all estimators are treated equal.
             Default value: None.
 
+        calculate_covariance : bool, optional
+            Whether the covariance matrix is calculated. Default value: True.
+
         return_individual_predictions : bool, optional
             Whether the individual estimator predictions are returned. Default value: False.
 
@@ -1203,10 +1207,10 @@ class EnsembleForge:
             `(n_thetas, n_x)`, otherwise, it has shape `(n_samples,)`). If more then one value vote_expectation_weight
             is given, this is a list with results for all entries in vote_expectation_weight.
 
-        covariance : ndarray or list of ndarray
+        covariance : None or ndarray or list of ndarray
             The covariance matrix of the (flattened) predictions, defined as the ensemble covariance. If more then one
             value vote_expectation_weight is given, this is a list with results
-            for all entries in vote_expectation_weight.
+            for all entries in vote_expectation_weight. If calculate_covariance is False, None is returned.
 
         weights : ndarray or list of ndarray
             Only returned if return_individual_predictions is True. The estimator weights `w_i`. If more then one value
@@ -1263,8 +1267,12 @@ class EnsembleForge:
             mean = np.average(predictions, axis=0, weights=these_weights)
             means.append(mean)
 
-            predictions_flat = predictions.reshape((predictions.shape[0], -1))
-            covariance = np.cov(predictions_flat.T, aweights=these_weights)
+            if calculate_covariance:
+                predictions_flat = predictions.reshape((predictions.shape[0], -1))
+
+                covariance = np.cov(predictions_flat.T, aweights=these_weights)
+            else:
+                covariance = None
 
             covariances.append(covariance)
 

madminer/plotting.py
@@ -342,26 +342,25 @@ def plot_fisher_information_contours_2d(
     # Grid
     xi = np.linspace(xrange[0], xrange[1], resolution)
     yi = np.linspace(yrange[0], yrange[1], resolution)
-    xx, yy = np.meshgrid(xi, yi)
+    xx, yy = np.meshgrid(xi, yi, indexing='xy')
     xx, yy = xx.flatten(), yy.flatten()
     thetas = np.vstack((xx, yy)).T
 
+    # fisher_distances_squared = np.matmul(
+    #     fisher_information_matrices[:, np.newaxis, :, :],  # (n_matrices, 1, m, n)
+    #     thetas[np.newaxis, :, :, np.newaxis],  # (1, n_grid, n, 1)
+    # )
+    # fisher_distances_squared = np.matmul(
+    #     thetas[np.newaxis, :, np.newaxis, :],  # (1, n_grid, 1, m)
+    #     fisher_distances_squared,  # (n_matrices, n_grid, m, 1)
+    # )
+    # fisher_distances_squared = fisher_distances_squared.reshape((n_matrices, resolution, resolution))
+    #
+    # logging.debug("Fisher distances: \n %s", fisher_distances_squared)
+
     # Calculate Fisher distances
-    fisher_distances_squared = np.matmul(
-        fisher_information_matrices[:, np.newaxis, :, :],  # (n_matrices, 1, m, n)
-        thetas[np.newaxis, :, :, np.newaxis],  # (1, n_grid, n, 1)
-    )
-    fisher_distances_squared = np.matmul(
-        thetas[np.newaxis, :, np.newaxis, :],  # (1, n_grid, 1, m)
-        fisher_distances_squared,  # (n_matrices, n_grid, m, 1)
-    )
-    fisher_distances_squared = fisher_distances_squared.reshape((n_matrices, resolution, resolution))
-
-    logging.debug("Fisher distances: \n %s", fisher_distances_squared)
-
-    fisher_distances_alt = np.einsum("ni,mij,nj->mn", thetas, fisher_information_matrices, thetas)
-    fisher_distances_alt_squared = fisher_distances_alt.reshape((n_matrices, resolution, resolution))
-    logging.debug("Diff: %s", fisher_distances_squared - fisher_distances_alt_squared)
+    fisher_distances = np.einsum("ni,mij,nj->mn", thetas, fisher_information_matrices, thetas)
+    fisher_distances_squared = fisher_distances.reshape((n_matrices, resolution, resolution))
 
     # Calculate uncertainties of Fisher distances
     fisher_distances_squared_uncertainties = []
@@ -373,10 +372,9 @@ def plot_fisher_information_contours_2d(
         var = np.einsum("ni,nj,ijkl,nk,nl->n", thetas, thetas, inf_cov, thetas, thetas)
 
         uncertainties = (var ** 0.5).reshape((resolution, resolution))
+        fisher_distances_squared_uncertainties.append(uncertainties)
 
         logging.debug("Std: %s", uncertainties)
-
-        fisher_distances_squared_uncertainties.append(uncertainties)
 
     # Plot results
     fig = plt.figure(figsize=(5.0, 5.0))
@@ -395,7 +393,7 @@ def plot_fisher_information_contours_2d(
         cs = plt.contour(
             xi,
             yi,
-            fisher_distances_squared[i],
+            fisher_distances_squared[i].T,
             np.array([d2_threshold]),
             colors=colors[i],
             linestyles=linestyles[i],