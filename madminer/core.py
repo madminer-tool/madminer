799ec38800747dcbeaf12d1b4fa00daa4dd9e626
Johann Brehmer <mail@johannbrehmer.de>
2018-11-15 09:35:18 -0500

    Fixed wrong filename in MadMiner.run_multiple()

--- madminer/core.py
+++ madminer/core.py
@@ -1039,7 +1039,7 @@ class MadMiner:
                     self._run_mg_and_pythia(
                         mg_directory,
                         mg_process_directory,
-                        mg_commands_filename,
+                        mg_process_directory + "/" + mg_commands_filename,
                         mg_process_directory + "/" + new_run_card_file,
                         mg_process_directory + "/" + param_card_file,
                         mg_process_directory + "/" + reweight_card_file,
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    "numpy",
+    "numpy>=1.11.0",
     "h5py",
     "scipy>=1.0.0",
     "scikit-learn>=0.19.0",
