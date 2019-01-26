e8af5100eb09a0b36b37493e3c01c39d62086a1c
Johann Brehmer <mail@johannbrehmer.de>
2019-01-26 10:40:30 -0500

    Added null logger to avoid error when no logging setup is defined, updated splash screen

--- madminer/__init__.py
+++ madminer/__init__.py
@@ -2,14 +2,16 @@ from madminer.__version__ import __version__
 
 import logging
 
+logging.getLogger('madminer').addHandler(logging.NullHandler())
+
 logger = logging.getLogger(__name__)
 
 logger.info("")
-logger.info("------------------------------------------------------------")
-logger.info("|                                                          |")
-logger.info("|  MadMiner v{}|".format(__version__.ljust(46)))
-logger.info("|                                                          |")
-logger.info("|           Johann Brehmer, Kyle Cranmer, and Felix Kling  |")
-logger.info("|                                                          |")
-logger.info("------------------------------------------------------------")
+logger.info("------------------------------------------------------------------------")
+logger.info("|                                                                      |")
+logger.info("|  MadMiner v{}|".format(__version__.ljust(58)))
+logger.info("|                                                                      |")
+logger.info("|         Johann Brehmer, Felix Kling, Irina Espejo, and Kyle Cranmer  |")
+logger.info("|                                                                      |")
+logger.info("------------------------------------------------------------------------")
 logger.info("")
