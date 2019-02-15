Fixed nuisance score / information calculation, added keyword shuffle_sample to combine_and_shuffle()

madminer/fisherinformation.py
@@ -1348,6 +1348,7 @@ class FisherInformation:
 
         # Get differential xsec per event, and the derivative wrt to theta
         sigma = mdot(theta_matrix, weights_benchmarks)  # Shape (n_events,)
+        total_xsec = np.sum(sigma)
         inv_sigma = sanitize_array(1.0 / sigma)  # Shape (n_events,)
         dsigma = mdot(dtheta_matrix, weights_benchmarks)  # Shape (n_parameters, n_events)
 
@@ -1357,13 +1358,15 @@ class FisherInformation:
         # Nuisance parameter Fisher info
         if include_nuisance_parameters and self.include_nuisance_parameters:
             nuisance_a = self.nuisance_morpher.calculate_a(weights_benchmarks)  # Shape (n_nuisance_params, n_events)
+            expected_a = np.einsum("vx,x->v", nuisance_a, sigma / total_xsec)
+            nuisance_scores = nuisance_a - expected_a[:, np.newaxis]  # Shape (n_nuisance_params, n_events)
 
             # grad_i dsigma(x), where i is a nuisance parameter, is given by
             # sigma[np.newaxis, :] * a
 
-            fisher_info_nuisance = luminosity * np.einsum("n,in,jn->nij", sigma, nuisance_a, nuisance_a)
-            fisher_info_mix = luminosity * np.einsum("in,jn->nij", dsigma, nuisance_a)
-            fisher_info_mix_transposed = luminosity * np.einsum("in,jn->nji", dsigma, nuisance_a)
+            fisher_info_nuisance = luminosity * np.einsum("n,in,jn->nij", sigma, nuisance_scores, nuisance_scores)
+            fisher_info_mix = luminosity * np.einsum("in,jn->nij", dsigma, nuisance_scores)
+            fisher_info_mix_transposed = luminosity * np.einsum("in,jn->nji", dsigma, nuisance_scores)
 
             n_all_parameters = self.n_parameters + self.n_nuisance_parameters
             fisher_info = np.zeros((fisher_info_phys.shape[0], n_all_parameters, n_all_parameters))

madminer/sampling.py
@@ -15,7 +15,7 @@ from madminer.utils.various import format_benchmark, create_missing_folders, shu
 logger = logging.getLogger(__name__)
 
 
-def combine_and_shuffle(input_filenames, output_filename, k_factors=None, overwrite_existing_file=True):
+def combine_and_shuffle(input_filenames, output_filename, k_factors=None, overwrite_existing_file=True, shuffle_sample=True):
     """
     Combines multiple MadMiner files into one, and shuffles the order of the events.
 
@@ -37,6 +37,9 @@ def combine_and_shuffle(input_filenames, output_filename, k_factors=None, overwr
 
     overwrite_existing_file : bool, optional
         If True and if the output file exists, it is overwritten. Default value: True.
+
+    shuffle_sample : bool, optional
+        If True, the output shuffle will be shuffled. Default value: True.
 
     Returns
     -------
@@ -86,7 +89,8 @@ def combine_and_shuffle(input_filenames, output_filename, k_factors=None, overwr
                 all_weights = np.vstack((all_weights, k_factor * weights))
 
     # Shuffle
-    all_observations, all_weights = shuffle(all_observations, all_weights)
+    if shuffle_sample:
+        all_observations, all_weights = shuffle(all_observations, all_weights)
 
     # Save result
     save_preformatted_events_to_madminer_file(