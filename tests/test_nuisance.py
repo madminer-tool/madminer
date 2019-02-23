ab7ddb8e4b41b0e806da880d2dc31f3319c276dc
Johann Brehmer <mail@johannbrehmer.de>
2019-02-23 13:13:35 -0500

    Fixed test_nuisance.py

--- tests/test_nuisance.py
+++ tests/test_nuisance.py
@@ -5,7 +5,7 @@ import numpy as np
 from collections import OrderedDict
 
 from madminer.core import MadMiner
-from madminer.lhe import LHEProcessor
+from madminer.lhe import LHEReader
 from madminer.fisherinformation import FisherInformation, profile_information
 
 
