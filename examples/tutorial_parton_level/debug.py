4d70460a113f3f97123c5ab7d5c1b8bc12899f9d
Johann Brehmer <mail@johannbrehmer.de>
2019-04-03 14:49:28 -0400

    Parallel sampling

--- examples/tutorial_parton_level/debug.py
+++ examples/tutorial_parton_level/debug.py
@@ -6,7 +6,7 @@ import logging
 logging.basicConfig(
     format='%(asctime)-5.5s %(name)-20.20s %(levelname)-7.7s %(message)s',
     datefmt='%H:%M',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 
 from madminer import sampling
@@ -14,26 +14,12 @@ from madminer.sampling import SampleAugmenter
 from madminer.fisherinformation import FisherInformation
 from madminer.ml import ScoreEstimator
 
-if False:
-    sampler = SampleAugmenter('data/madminer_example_systematics_with_data.h5')
-    x, theta, t_xz, _ = sampler.sample_train_local(
-        theta=sampling.benchmark('sm'),
-        n_samples=1000,
-    )
+sampler = SampleAugmenter('data/madminer_example_systematics_with_data.h5')
 
-    estimator = ScoreEstimator(n_hidden=(20,))
-    estimator.train(
-        method='sally',
-        x='data/samples/x_train.npy',
-        t_xz='data/samples/t_xz_train.npy',
-    )
-    estimator.save('models/debug')
-
-fisher = FisherInformation('data/madminer_example_systematics_with_data.h5', include_nuisance_parameters=False)
-
-info = fisher.calculate_fisher_information_hist1d(
-    theta=[0.,0.],
-    observable='pt_j1',
-    luminosity=3000000.,
-    nbins=10
+x, theta0, theta1, y, r_xz, t_xz, n_effective_samples = sampler.sample_train_ratio(
+    theta0=sampling.random_morphing_points(None, [('gaussian', 0., 15.), ('gaussian', 0., 15.)]),
+    theta1=sampling.benchmark('sm'),
+    nu0=sampling.iid_nuisance_parameters(),
+    nu1=sampling.nominal_nuisance_parameters(),
+    n_samples=10000,
 )
