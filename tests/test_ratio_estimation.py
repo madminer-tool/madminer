687b82bf64cf6f39d6f3855904f178001009c8f3
Johann Brehmer <mail@johannbrehmer.de>
2019-04-23 13:30:14 -0400

    Updated ratio estimation test

--- tests/test_ratio_estimation.py
+++ tests/test_ratio_estimation.py
@@ -2,7 +2,18 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import os
 import numpy as np
+import logging
 from scipy.stats import norm
+
+# MadMiner output
+logging.basicConfig(
+    format="%(asctime)-5.5s %(name)-20.20s %(levelname)-7.7s %(message)s", datefmt="%H:%M", level=logging.INFO
+)
+
+# Output of all other modules (e.g. matplotlib)
+for key in logging.Logger.manager.loggerDict:
+    if "madminer" not in key:
+        logging.getLogger(key).setLevel(logging.WARNING)
 
 from madminer.ml import ParameterizedRatioEstimator
 
@@ -11,10 +22,6 @@ if not os.path.exists("tests/data"):
 
 
 # Simulator settings
-def z_mean(theta):
-    return theta + 10.0
-
-
 z_std = 2.0
 x_std = 1.0
 
@@ -22,19 +29,19 @@ x_std = 1.0
 # Simulator
 def simulate(theta, theta0=None, theta1=None, theta_score=None, npoints=None):
     # Draw latent variable
-    z = np.random.normal(loc=z_mean(theta), scale=z_std, size=npoints)
+    z = np.random.normal(loc=theta, scale=z_std, size=npoints)
 
     # Draw observable
     x = np.random.normal(loc=z, scale=x_std, size=None)
 
     # Calculate joint likelihood ratio and joint score
     if theta0 is not None and theta1 is not None:
-        r_xz = norm(loc=z_mean(theta0), scale=z_std).pdf(z) / norm(loc=z_mean(theta1), scale=z_std).pdf(z)
+        r_xz = norm(loc=theta0, scale=z_std).pdf(z) / norm(loc=theta1, scale=z_std).pdf(z)
     else:
         r_xz = None
 
     if theta_score is not None:
-        t_xz = (x - z_mean(theta_score)) / z_std ** 2
+        t_xz = (x - theta_score) / z_std ** 2
     else:
         t_xz = None
 
@@ -44,13 +51,13 @@ def simulate(theta, theta0=None, theta1=None, theta_score=None, npoints=None):
 # True likeleihood ratio function
 def calculate_likelihood_ratio(x, theta0, theta1=0.0):
     combined_std = (z_std ** 2 + x_std ** 2) ** 0.5
-    r_x = norm(loc=z_mean(theta0), scale=combined_std).pdf(x) / norm(loc=z_mean(theta1), scale=combined_std).pdf(x)
+    r_x = norm(loc=theta0, scale=combined_std).pdf(x) / norm(loc=theta1, scale=combined_std).pdf(x)
     return r_x
 
 
-def run_test():
+def generate_data(sample_sizes):
     # Run simulator and generate etraining data
-    n_param_points = 5000  # number of parameter points to train
+    n_param_points = max(sample_sizes) // 2  # number of parameter points to train
 
     theta0 = np.random.uniform(low=-4.0, high=4.0, size=n_param_points)  # numerator, uniform prior
     theta1 = np.zeros(shape=n_param_points)  # denominator: fixed at 0
@@ -75,26 +82,27 @@ def run_test():
     np.save("tests/data/r_xz_train.npy", r_xz_train)
     np.save("tests/data/t_xz_train.npy", t_xz_train)
 
+
+def run_test(method, alpha, sample_size):
     # Train model
-    estimator = ParameterizedRatioEstimator(n_hidden=(20, 20))
+    estimator = ParameterizedRatioEstimator(n_hidden=(50, 50))
     estimator.train(
-        method="alices",
+        method=method,
         x="tests/data/x_train.npy",
         y="tests/data/y_train.npy",
         theta="tests/data/theta0_train.npy",
         r_xz="tests/data/r_xz_train.npy",
         t_xz="tests/data/t_xz_train.npy",
-        alpha=0.1,
-        n_epochs=10,
-        validation_split=None,
-        batch_size=256,
+        alpha=alpha,
+        limit_samplesize=sample_size,
+        verbose="few",
     )
 
     # Generate evaluation data
-    n_param_points_test = 100
-    n_thetas_grid = 100
+    n_x_test = 50
+    n_thetas_grid = 20
 
-    theta_test = 1.0 * np.ones(shape=n_param_points_test).reshape(-1, 1)
+    theta_test = 1.0 * np.ones(shape=n_x_test).reshape(-1, 1)
     x_test, _, _ = simulate(theta_test)
     np.save("tests/data/x_test.npy", x_test)
 
@@ -106,12 +114,19 @@ def run_test():
     log_r_test_true = []
     for theta in theta_grid:
         log_r_test_true.append(np.log(calculate_likelihood_ratio(x_test, theta)))
-    log_r_test_true = np.array(log_r_test_true)
+    log_r_test_true = np.array(log_r_test_true).reshape(n_thetas_grid, n_x_test)
 
     # Evaluation
     log_r_tests_alices, _ = estimator.evaluate(
         theta="tests/data/theta_grid.npy", x="tests/data/x_test.npy", evaluate_score=False
     )
+
+    logging.info("Log likelihood ratio results for method %s with %s samples:", method, sample_size)
+    logging.info(" theta  |  true  |  est. ")
+    logging.info("----------------------------")
+    for theta, all_true, all_est in zip(theta_grid, log_r_test_true, log_r_tests_alices):
+        for true, est in zip(all_true[:1], all_est[:1]):
+            logging.info(" {:5.2f}  |  {:4.1f}  |  {:4.1f} ".format(theta[0], true, est))
 
     # Calculate error
     rmse = np.mean((log_r_test_true - log_r_tests_alices) ** 2) ** 0.5
@@ -120,12 +135,35 @@ def run_test():
 
 
 def test_ratio_estimation():
-    rmse = run_test()
+    methods = ["carl", "rolr", "alice", "cascal", "rascal", "alices"]
+    alphas = [0.0, 0.0, 0.0, 1.0, 1.0, 1.0]
+    sample_sizes = [100, 100000]
+    rmses = []
 
-    print("Root mean squared error of true log r vs ALICES log r: {}".format(rmse))
+    print("Generating data for ratio estimation")
+    generate_data(sample_sizes)
 
-    assert rmse < 1000.0
+    for method, alpha in zip(methods, alphas):
+        this_rmses = []
+        for sample_size in sample_sizes:
+            print("Training method {} on {} samples".format(method, sample_size))
+            this_rmses.append(run_test(method, alpha, sample_size))
+            print(this_rmses[-1])
+        rmses.append(this_rmses)
+    rmses = np.asarray(rmses)
+
+    print("")
+    print("Results: Root mean squared error of log r")
+    print("")
+    print(" Method  |   100 samples  |  100k samples ")
+    print("------------------------------------------")
+    for method, this_rmses in zip(methods, rmses):
+        print(" {:>6s}  |    {:12.3f}  |  {:12.3f} ".format(method, this_rmses[0], this_rmses[1]))
+
+    print("")
+
+    assert np.max(rmses[:, -1]) < 10.0
 
 
 if __name__ == "__main__":
-    test_toy_workflow()
+    test_ratio_estimation()
