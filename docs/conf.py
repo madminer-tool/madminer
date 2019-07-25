efb3c5b8636b2541a07fe2406b120cfb9514c1ac
Johann Brehmer <mail@johannbrehmer.de>
2019-07-25 09:20:19 -0400

    Bumped version number

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = 'Johann Brehmer, Felix Kling, Irina Espejo, and Kyle Cranmer 2018'
 author = 'Johann Brehmer, Felix Kling, Irina Espejo, and Kyle Cranmer'
 
 # The short X.Y version
-version = '0.4.8'
+version = '0.4.9'
 # The full version, including alpha/beta/rc tags
 release = version
 
