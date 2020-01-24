a3007748d4e127338840f2f32be7e179aa16da8c
Johann Brehmer <mail@johannbrehmer.de>
2020-01-24 09:56:52 -0500

    Code style

--- madminer/fisherinformation.py
+++ madminer/fisherinformation.py
@@ -15,6 +15,7 @@ from madminer.ml import ParameterizedRatioEstimator, ScoreEstimator, Ensemble, l
 
 logger = logging.getLogger(__name__)
 
+
 class FisherInformation(DataAnalyzer):
     """
     Functions to calculate expected Fisher information matrices.
@@ -363,8 +364,7 @@ class FisherInformation(DataAnalyzer):
                     )
                 else:
                     fisher_info_kin = model.calculate_fisher_information(
-                        x=unweighted_x_sample_file, n_events=luminosity * total_xsec,
-                        theta=theta,
+                        x=unweighted_x_sample_file, n_events=luminosity * total_xsec, theta=theta
                     )
                     covariance = None
 
@@ -1678,8 +1678,9 @@ def profile_information(
 
     return profiled_information
 
-class InformationGeometry():
-    
+
+class InformationGeometry:
+
     """
     Functions to calculate limits using Information Geometry.
         
@@ -1695,15 +1696,14 @@ class InformationGeometry():
     calculates the distance contours and equivalently the p-values throughout parameter space.
         
     """
-    
+
     def __init__(self):
-        self.infotype  = None
+        self.infotype = None
         self.dimension = 0
         self.information_formula = None
-        self.inverse = 'exact'
-    
-    
-    def information_from_formula(self,formula,dimension):
+        self.inverse = "exact"
+
+    def information_from_formula(self, formula, dimension):
         """
         Explicitly defines the Fisher Information as function of the theory parameter `theta`
         through a formula that can be avaulated using `eval()`.
@@ -1718,13 +1718,12 @@ class InformationGeometry():
         dimension : int
             Dimensionality of the theory parameter space.
         """
-        
+
         self.infotype = "formula"
         self.dimension = dimension
         self.information_formula = formula
-    
-    
-    def information_from_grid(self,theta_grid, fisherinformation_grid,option='smooth',inverse='exact'):
+
+    def information_from_grid(self, theta_grid, fisherinformation_grid, option="smooth", inverse="exact"):
         """
         Loads a grid of coordinates and corresponding Fisher Information, which is then interpolated.
             
@@ -1747,32 +1746,32 @@ class InformationGeometry():
             the Fisher Information and then inverting it ("exact") or by first inverting the grid
             of Fisher Informations and then interpolating the inverse ("interpolate"). Default = 'exact'.
         """
-        
+
         self.infotype = "grid"
         self.inverse = inverse
-        
-        #load from file
-        theta_grid=load_and_check(theta_grid)
-        fisherinformation_grid=load_and_check(fisherinformation_grid)
+
+        # load from file
+        theta_grid = load_and_check(theta_grid)
+        fisherinformation_grid = load_and_check(fisherinformation_grid)
         self.dimension = len(fisherinformation_grid[0])
-        
-        #Interpolate Information
-        if option=='linear':
-            self.infofunction=LinearNDInterpolator(points=theta_grid,values=np.array(fisherinformation_grid))
-        elif option=='smooth':
-            self.infofunction=CloughTocher2DInterpolator(points=theta_grid,values=np.array(fisherinformation_grid))
+
+        # Interpolate Information
+        if option == "linear":
+            self.infofunction = LinearNDInterpolator(points=theta_grid, values=np.array(fisherinformation_grid))
+        elif option == "smooth":
+            self.infofunction = CloughTocher2DInterpolator(points=theta_grid, values=np.array(fisherinformation_grid))
         else:
-            RuntimeError("Option %s unknown",option)
+            RuntimeError("Option %s unknown", option)
 
-        #Interpolate inverse information
-        if self.inverse == 'interpolate':
-            inv_fisherinformation_grid =  np.array([np.linalg.inv(info) for info in fisherinformation_grid])
-            if option=='linear':
-                self.infofunction_inv=LinearNDInterpolator(points=theta_grid,values=inv_fisherinformation_grid)
-            elif option=='smooth':
-                self.infofunction_inv=CloughTocher2DInterpolator(points=theta_grid,values=inv_fisherinformation_grid)
+        # Interpolate inverse information
+        if self.inverse == "interpolate":
+            inv_fisherinformation_grid = np.array([np.linalg.inv(info) for info in fisherinformation_grid])
+            if option == "linear":
+                self.infofunction_inv = LinearNDInterpolator(points=theta_grid, values=inv_fisherinformation_grid)
+            elif option == "smooth":
+                self.infofunction_inv = CloughTocher2DInterpolator(points=theta_grid, values=inv_fisherinformation_grid)
 
-    def _information(self,theta):
+    def _information(self, theta):
         """
         Low level function that calculates the Fisher Information as function of
         the theory parameter `theta`
@@ -1787,26 +1786,27 @@ class InformationGeometry():
         fisher_information : ndarray
             Fisher information matrix with shape `(n_dimension, n_dimension)`.
         """
-        
-        #check input format
-        assert len(theta) == self.dimension, \
-            "theta should have length %r, not %r" % (self.dimension,len(theta))
-        
-        #calculate information
+
+        # check input format
+        assert len(theta) == self.dimension, "theta should have length %r, not %r" % (self.dimension, len(theta))
+
+        # calculate information
         if self.infotype == "formula":
             information = eval(self.information_formula)
         elif self.infotype == "grid":
             information = self.infofunction(tuple(theta))
         else:
             raise RuntimeError("Information not defined yet")
-        
-        #check output format
-        assert np.shape(information)==(self.dimension,self.dimension), \
-            "information should have shape %r, not %r" % ((self.dimension,self.dimension),np.shape(information))
-        
+
+        # check output format
+        assert np.shape(information) == (self.dimension, self.dimension), "information should have shape %r, not %r" % (
+            (self.dimension, self.dimension),
+            np.shape(information),
+        )
+
         return information
-                                                        
-    def _information_inv(self,theta):
+
+    def _information_inv(self, theta):
         """
         Low level function that calculates the inverse Fisher Information as function of
         the theory parameter `theta`.
@@ -1822,14 +1822,13 @@ class InformationGeometry():
         inverse_fisher_information : ndarray
             Inverse Fisher information matrix with shape `(n_dimension, n_dimension)`.
         """
-        
-        if self.inverse == 'interpolate':
+
+        if self.inverse == "interpolate":
             return self.infofunction_inv(tuple(theta))
         else:
             return np.linalg.inv(self._information(theta))
-                            
-    
-    def _information_derivative(self,theta):
+
+    def _information_derivative(self, theta):
         """
         Low level function that calculates the derivative of Fisher Information
         `\partial_k I_{ij}` at the theory parameter `theta`.
@@ -1845,12 +1844,14 @@ class InformationGeometry():
         fisher_information_derivative : ndarray
             Derivative of Fisher information matrix with shape `(n_dimension, n_dimension, n_dimension)`.
         """
-        
-        epsilon=10**-3
-        dtheta=np.identity(len(theta))*epsilon
-        return np.array([(self._information(theta+dtheta[k])-self._information(theta)) / epsilon for k in range(len(theta))])
-                        
-    def _christoffel(self,theta):
+
+        epsilon = 10 ** -3
+        dtheta = np.identity(len(theta)) * epsilon
+        return np.array(
+            [(self._information(theta + dtheta[k]) - self._information(theta)) / epsilon for k in range(len(theta))]
+        )
+
+    def _christoffel(self, theta):
         """
         Low level function that calculates the Christoffel symbol (2nd kind) Gamma^i_jk at
         the theory parameter `theta`.  Here Gamma^i_jk=0.5*I^{im}(\partial_k I_{mj}
@@ -1866,14 +1867,13 @@ class InformationGeometry():
         christoffel_symbol : ndarray
             Christoffel symbol with shape `(n_dimension, n_dimension, n_dimension)`.
         """
-        
-        term1= np.einsum("ad,cdb->abc", self._information_inv(theta), self._information_derivative(theta))
-        term2= np.einsum("ad,bdc->abc", self._information_inv(theta), self._information_derivative(theta))
-        term3= np.einsum("ad,bcd->abc", self._information_inv(theta), self._information_derivative(theta))
-        return  0.5 * (term1+term2-term3)
-        
-                            
-    def find_trajectory(self,theta0,dtheta0,limits,stepsize=1):
+
+        term1 = np.einsum("ad,cdb->abc", self._information_inv(theta), self._information_derivative(theta))
+        term2 = np.einsum("ad,bdc->abc", self._information_inv(theta), self._information_derivative(theta))
+        term3 = np.einsum("ad,bcd->abc", self._information_inv(theta), self._information_derivative(theta))
+        return 0.5 * (term1 + term2 - term3)
+
+    def find_trajectory(self, theta0, dtheta0, limits, stepsize=1):
         """
         Finds the geodesic trajectory starting at a parameter point theta0 going in the
         initial direction dtheta0.
@@ -1904,47 +1904,48 @@ class InformationGeometry():
             List of distances from the staring point theta0 `(n_points, )`.
             
         """
-        
-        #initiate starting point
-        theta = 1.*theta0
-        dtheta = 1.*dtheta0
-        dist = 0
-        output_theta=[1.*theta]
-        output_dist=[0]
 
-        #calculate free-fall trajectory
-        counter=0
-        in_grid=True
-        while in_grid and counter<200:
-            counter+=1
-            #normalize dtheta to stepsize
-            dtheta = dtheta/np.linalg.norm(dtheta)
-            
-            #calculate ddtheta and distance
-            ddtheta = -1.* np.einsum("abc,b,c->a", self._christoffel(theta), dtheta, dtheta)
+        # initiate starting point
+        theta = 1.0 * theta0
+        dtheta = 1.0 * dtheta0
+        dist = 0
+        output_theta = [1.0 * theta]
+        output_dist = [0]
+
+        # calculate free-fall trajectory
+        counter = 0
+        in_grid = True
+        while in_grid and counter < 200:
+            counter += 1
+            # normalize dtheta to stepsize
+            dtheta = dtheta / np.linalg.norm(dtheta)
+
+            # calculate ddtheta and distance
+            ddtheta = -1.0 * np.einsum("abc,b,c->a", self._christoffel(theta), dtheta, dtheta)
             ddist = np.sqrt(np.einsum("ab,a,b", self._information(theta), dtheta, dtheta))
-            
-            #determine stepsize to be used
-            max_stepsize = 0.05 * np.linalg.norm(dtheta)  / np.linalg.norm(ddtheta)
-            use_stepsize = min(max_stepsize,stepsize)
-            
-            #update theta,dtheta, dist
+
+            # determine stepsize to be used
+            max_stepsize = 0.05 * np.linalg.norm(dtheta) / np.linalg.norm(ddtheta)
+            use_stepsize = min(max_stepsize, stepsize)
+
+            # update theta,dtheta, dist
             theta += dtheta * use_stepsize
             dtheta += ddtheta * use_stepsize
             dist += ddist * use_stepsize
-            
-            #save
-            theta = np.array(theta)
-            if np.isnan(dist):  break
-            output_theta.append(theta*1.)
-            output_dist.append(dist*1.)
 
-            #check if outside range
-            for th,lim in zip(theta,limits):
-                if (th<lim[0] or th>lim[1]): in_grid=False
-        
-        return np.array(output_theta),output_dist
-                                                                                                        
+            # save
+            theta = np.array(theta)
+            if np.isnan(dist):
+                break
+            output_theta.append(theta * 1.0)
+            output_dist.append(dist * 1.0)
+
+            # check if outside range
+            for th, lim in zip(theta, limits):
+                if th < lim[0] or th > lim[1]:
+                    in_grid = False
+
+        return np.array(output_theta), output_dist
 
     def distance_contours(
         self,
@@ -2007,57 +2008,55 @@ class InformationGeometry():
             staring point theta0 `(n_points, )`.
             
         """
-        
-        #automatic setting of stepsize and ntrajectories
-        if stepsize==None:
-            stepsize = min([(limit[1]-limit[0])/20. for limit in grid_ranges])
-        if ntrajectories==None:
-            ntrajectories = 20*self.dimension
+
+        # automatic setting of stepsize and ntrajectories
+        if stepsize == None:
+            stepsize = min([(limit[1] - limit[0]) / 20.0 for limit in grid_ranges])
+        if ntrajectories == None:
+            ntrajectories = 20 * self.dimension
         if self.dimension is not 2:
             continous_sampling = False
-        limits=(1.+2.*stepsize)*np.array(grid_ranges)
-        
-        #determine trajectories
-        thetas= []
-        distances=[]
+        limits = (1.0 + 2.0 * stepsize) * np.array(grid_ranges)
+
+        # determine trajectories
+        thetas = []
+        distances = []
         for i in range(ntrajectories):
             if continous_sampling:
-                angle = 2.*np.pi/float(ntrajectories)*i
-                dth0=np.array([np.cos(angle),np.sin(angle)])
+                angle = 2.0 * np.pi / float(ntrajectories) * i
+                dth0 = np.array([np.cos(angle), np.sin(angle)])
             else:
-                dth0=np.array([random.uniform(-1,1) for _ in range(self.dimension)])
-            logger.debug("Calculate Trajectory Number %s with dtheta0=%s", i,dth0)
-            ths,ds=self.find_trajectory(theta0,dth0,limits,stepsize)
+                dth0 = np.array([random.uniform(-1, 1) for _ in range(self.dimension)])
+            logger.debug("Calculate Trajectory Number %s with dtheta0=%s", i, dth0)
+            ths, ds = self.find_trajectory(theta0, dth0, limits, stepsize)
             for th in ths:
                 thetas.append(th)
             for d in ds:
                 distances.append(d)
-        thetas=np.array(thetas)
+        thetas = np.array(thetas)
 
-        #Create Theta Grid
-        theta_grid_each = self._make_theta_grid_each(grid_ranges,grid_resolutions)
-        theta_grid = self._make_theta_grid(grid_ranges,grid_resolutions)
-        
-        #Create Distance Grid
-        distance_grid = griddata(thetas, distances, (theta_grid_each[0],theta_grid_each[1]), method='linear')
-        
-        #Create p-value Grid
+        # Create Theta Grid
+        theta_grid_each = self._make_theta_grid_each(grid_ranges, grid_resolutions)
+        theta_grid = self._make_theta_grid(grid_ranges, grid_resolutions)
+
+        # Create Distance Grid
+        distance_grid = griddata(thetas, distances, (theta_grid_each[0], theta_grid_each[1]), method="linear")
+
+        # Create p-value Grid
         p_value_grid = self._asymptotic_p_value(distance_grid)
 
-        #return
+        # return
         if return_trajectories:
-            return theta_grid, p_value_grid, distance_grid, (thetas,distances)
+            return theta_grid, p_value_grid, distance_grid, (thetas, distances)
         else:
             return theta_grid, p_value_grid, distance_grid
-                                                                                                                                    
 
-    def _make_theta_grid_each(self,grid_ranges, grid_resolutions):
+    def _make_theta_grid_each(self, grid_ranges, grid_resolutions):
         theta_each = []
         for resolution, (theta_min, theta_max) in zip(grid_resolutions, grid_ranges):
             theta_each.append(np.linspace(theta_min, theta_max, resolution))
         theta_grid_each = np.meshgrid(*theta_each, indexing="ij")
         return theta_grid_each
-
 
     def _make_theta_grid(self, grid_ranges, grid_resolutions):
         theta_grid_each = self._make_theta_grid_each(grid_ranges, grid_resolutions)
@@ -2065,10 +2064,9 @@ class InformationGeometry():
         theta_grid = np.vstack(theta_grid_each).T
         return theta_grid
 
-
-    def _asymptotic_p_value(self,dist):
+    def _asymptotic_p_value(self, dist):
         """
         Low level function to convert distances in p-values
         """
-        p_value = chi2.sf(x=dist*dist, df=self.dimension)
+        p_value = chi2.sf(x=dist * dist, df=self.dimension)
         return p_value
