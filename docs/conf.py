77a7e71e33c3c88b599bce338a5e872e210bc53e
Sinclert Pérez <Sinclert@hotmail.com>
2020-04-27 18:03:28 +0200

    Bump up version to 0.7.2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = 'Johann Brehmer, Felix Kling, Irina Espejo, and Kyle Cranmer 2018-20
 author = 'Johann Brehmer, Felix Kling, Irina Espejo, and Kyle Cranmer'
 
 # The short X.Y version
-version = '0.7.1'
+version = '0.7.2'
 # The full version, including alpha/beta/rc tags
 release = version
 
