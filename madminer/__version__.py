f2bbacc6fbe9c105ed631d36e142686962b397f6
Sinclert Pérez <Sinclert@hotmail.com>
2020-07-01 17:47:56 +0200

    Fix VERSION paths

--- madminer/__version__.py
+++ madminer/__version__.py
@@ -1,1 +1,6 @@
-__version__ = open('../VERSION').read()
+import os
+
+project_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+version_file = os.path.join(project_folder, 'VERSION')
+
+__version__ = open(version_file).read()
