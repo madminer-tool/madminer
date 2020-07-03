abfe919a074e2d65d582de9d4e011020e3d08384
Sinclert Pérez <Sinclert@hotmail.com>
2020-07-03 20:09:05 +0200

    Make setup.py Python2 compatible

--- setup.py
+++ setup.py
@@ -62,12 +62,8 @@ EXTRAS["examples"] = sorted(
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
+with open(os.path.join(here, 'README.md')) as f:
+    long_description = '\n' + f.read()
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
