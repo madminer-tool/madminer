3b44c22fc423500af19e333773588c41de8f7895
Johann Brehmer <mail@johannbrehmer.de>
2020-07-22 17:16:45 -0400

    Implemented plot_1d_morphing_basis()

--- madminer/plotting/__init__.py
+++ madminer/plotting/__init__.py
@@ -1,5 +1,5 @@
 from .distributions import plot_distributions, plot_histograms
-from .morphing import plot_2d_morphing_basis, plot_nd_morphing_basis_scatter, plot_nd_morphing_basis_slices
+from .morphing import plot_2d_morphing_basis, plot_nd_morphing_basis_scatter, plot_nd_morphing_basis_slices, plot_1d_morphing_basis
 from .fisherinformation import (
     plot_fisherinfo_barplot,
     plot_fisher_information_contours_2d,
