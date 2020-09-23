8b9d2e0857016d5ce3428d33822ea1dade915915
Sinclert Pérez <Sinclert@hotmail.com>
2020-09-23 10:15:20 -0400

    src: ML module f-strings

--- madminer/ml/double_parameterized_ratio.py
+++ madminer/ml/double_parameterized_ratio.py
@@ -309,13 +309,9 @@ class DoubleParameterizedRatioEstimator(ConditionalEstimator):
             self.n_parameters = n_parameters
 
         if n_parameters != self.n_parameters:
-            raise RuntimeError(
-                "Number of parameters does not match model: {} vs {}".format(n_parameters, self.n_parameters)
-            )
+            raise RuntimeError(f"Number of parameters does not match: {n_parameters} vs {self.n_parameters}")
         if n_observables != self.n_observables:
-            raise RuntimeError(
-                "Number of observables does not match model: {} vs {}".format(n_observables, self.n_observables)
-            )
+            raise RuntimeError(f"Number of observables does not match: {n_observables} vs {self.n_observables}")
 
         # Data
         data = self._package_training_data(method, x, theta0, theta1, y, r_xz, t_xz0, t_xz1)
@@ -499,9 +495,9 @@ class DoubleParameterizedRatioEstimator(ConditionalEstimator):
     @staticmethod
     def _check_required_data(method, r_xz, t_xz0, t_xz1):
         if method in ["cascal", "alices", "rascal"] and (t_xz0 is None or t_xz1 is None):
-            raise RuntimeError("Method {} requires joint score information".format(method))
+            raise RuntimeError(f"Method {method} requires joint score information")
         if method in ["rolr", "alice", "alices", "rascal"] and r_xz is None:
-            raise RuntimeError("Method {} requires joint likelihood ratio information".format(method))
+            raise RuntimeError(f"Method {method} requires joint likelihood ratio information")
 
     @staticmethod
     def _package_training_data(method, x, theta0, theta1, y, r_xz, t_xz0, t_xz1):
@@ -527,4 +523,4 @@ class DoubleParameterizedRatioEstimator(ConditionalEstimator):
 
         estimator_type = str(settings["estimator_type"])
         if estimator_type != "double_parameterized_ratio":
-            raise RuntimeError("Saved model is an incompatible estimator type {}.".format(estimator_type))
+            raise RuntimeError(f"Saved model is an incompatible estimator type {estimator_type}.")
