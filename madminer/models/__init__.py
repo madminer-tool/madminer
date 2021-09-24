d98cf3819237eccdf581e78066e02c3fc0c8ad39
Sinclert Pérez <Sinclert@hotmail.com>
2021-09-24 10:59:39 -0400

    models: define AnalysisParam data class

--- madminer/models/__init__.py
+++ madminer/models/__init__.py
@@ -1,6 +1,8 @@
 from .benchmarks import Benchmark
 from .benchmarks import FiniteDiffBenchmark
 
+from .parameters import AnalysisParameter
+
 from .readers import Cut
 from .readers import Efficiency
 from .readers import Observable
